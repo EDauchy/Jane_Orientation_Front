1a4f8dc3968a2fd43f8983b485981966df1fcc83
NathanCartegnie <nathan.cartegnie@ynov.com>
2026-02-07 21:40:18 +0100

    feat: add dynamic map component with establishment support

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 import OrientationTest from './pages/OrientationTest';
 import Dashboard from './pages/Dashboard';
+import Map from "./components/Map.tsx";
 
 // Protected Route Component
 const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
@@ -29,6 +30,11 @@ function AppRoutes() {
       <Route path="/dashboard" element={
         <ProtectedRoute>
           <Dashboard />
+        </ProtectedRoute>
+      } />
+      <Route path="/map" element={
+        <ProtectedRoute>
+            <Map />
         </ProtectedRoute>
       } />
       <Route path="/" element={<Navigate to="/dashboard" />} />
